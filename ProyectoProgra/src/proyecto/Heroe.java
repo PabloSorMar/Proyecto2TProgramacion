ac76cf493e810b65c3bb68e785b2ab06770ca1a0
Ignacio Bobes <nagtar98@gmail.com>
2026-03-28 16:20:53 +0100

    completado combate y hecho que los personajes tengan una o dos armas

--- ProyectoProgra/src/proyecto/Heroe.java
+++ ProyectoProgra/src/proyecto/Heroe.java
@@ -13,4 +13,11 @@ this.arma2 = arma2;
 
     public Arma getArma2() { return this.arma2; }
     public void setArma2(Arma arma2) { this.arma2 = arma2; }
+
+    @Override
+    public void RealizarTurno(List<Entidad> aliados, List<Entidad> enemigos, Entidad personaje) {
+        // Por ahora, el Héroe realiza el turno exactamente igual que una Entidad (de forma aleatoria).
+        // Al usar super.RealizarTurno() estamos llamando a la función padre que ya contiene toda la lógica.
+        super.RealizarTurno(aliados, enemigos, personaje);
+    }
 }
--- ProyectoProgra/src/proyecto/Main.java
+++ ProyectoProgra/src/proyecto/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         List<Entidad> enemigos = new ArrayList<>();
         List<Entidad> aliados = new ArrayList<>();
@@ -13,7 +13,7 @@ public class Main {
         Armaduras armadura1 = new Armaduras("Ligera", 20, 20);
         Arma arma1 = new Arma("Espada Sierra", 20, 100, false, 2, 1, null, 1);
 
-        Habilidades hab1 = new Habilidades("hab1",2," ",true,"ofensiva");     
+        Habilidades hab1 = new Habilidades("hab1", 2, " ", true, "ofensiva");
         List<Habilidades> habilidades = new ArrayList<>();
         habilidades.add(hab1);
 
@@ -22,13 +22,51 @@ public class Main {
 
         aliados.add(aliado1);
         enemigos.add(enemigo1);
-        personajes.add(aliado1);personajes.add(enemigo1);
+        personajes.add(aliado1);
+        personajes.add(enemigo1);
+        while (aliados.size() > 0 && enemigos.size() > 0) {
+            for (int i = 0; i < personajes.size(); i++) {
+                Entidad perso = personajes.get(i);
 
-        for(Entidad perso:personajes)
-        {
-            perso.RealizarTurno(aliados, enemigos, perso);
+                // Solo si el personaje está vivo y la batalla no ha acabado
+                if (perso.getVida() > 0 && aliados.size() > 0 && enemigos.size() > 0) {
+                    perso.RealizarTurno(aliados, enemigos, perso);
+
+                    // Limpiamos los muertos de las listas aliado a aliado y enemigo a enemigo
+                    /*
+                     * Se recorre al revés (desde el final hacia el principio 0) para no afectar las
+                     * posiciones al borrar
+                     */
+                    for (int j = aliados.size() - 1; j >= 0; j--) {
+                        if (aliados.get(j).getVida() <= 0) {
+                            aliados.remove(j);
+                        }
+                    }
+
+                    for (int j = enemigos.size() - 1; j >= 0; j--) {
+                        if (enemigos.get(j).getVida() <= 0) {
+                            enemigos.remove(j);
+                        }
+                    }
+                }
+            }
+
+            // Finalmente, borramos también a los muertos de la lista general de turnos
+            for (int j = personajes.size() - 1; j >= 0; j--) {
+                if (personajes.get(j).getVida() <= 0) {
+                    personajes.remove(j);
+                }
+            }
         }
 
+        System.out.println("--- FIN DEL COMBATE ---");
+        if (aliados.size() > 0) {
+            System.out.println("¡Victoria para los Aliados!");
+        } else if (enemigos.size() > 0) {
+            System.out.println("¡Victoria para los Enemigos!");
+        } else {
+            System.out.println("Todos han muerto. Es un empate.");
+        }
 
-    } 
+    }
 }
